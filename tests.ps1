1997e006c3851c7e876bcc46582d1cec4979d498
Nigel van Keulen <nigel2392@gmail.com>
2025-06-23 10:02:27 +0200

    update powershell testing script

--- tests.ps1
+++ tests.ps1
@@ -1,27 +1,91 @@
 $dirname = Split-Path -Parent $MyInvocation.MyCommand.Path
 $dirname = Split-Path -Leaf $dirname
 
-# Reset test docker containers and volumes
+# Reset test docker containers
 docker-compose -f "./test-databases.docker-compose.yml" down
-docker volume rm "${dirname}_queries_mysql_data"
-docker volume rm "${dirname}_queries_mariadb_data"
-docker volume rm "${dirname}_queries_postgres_data"
-docker-compose -f "./test-databases.docker-compose.yml" up -d
 
 # Databases to test (these translate to Go build tags)
 $Databases = @(
     "sqlite",
     "mysql_local",
     "mysql",
-    "mariadb"#,
-    # "postgres"
+    "mariadb",
+    "postgres"
 )
 
+# Databases defined in docker-compose.yml
+$DockerDatabases = @{
+    "mysql"      = $true
+    "mariadb"    = $true
+    "postgres"   = $true
+}
+
+# Empty test array will be built out
+$testsToRun = @(
+
+)
+
+# Flags for the test run
+$flags = @{
+    verbose = $true
+    failslow = $false
+}
+
+# Check script arguments that were passed in
+foreach ($arg in $args) {
+    switch ($arg) {
+        "silent" {
+            $flags.verbose = $false
+            continue
+        }
+        "failslow" {
+            $flags.failslow = $true
+            continue
+        }
+        "down" {
+            # If the argument is "down", remove all volumes and exit
+            foreach ($db in $DockerDatabases.Keys) {
+                docker volume rm "${dirname}_queries_${db}_data" -f
+            }
+            exit 0
+        }
+        default {
+            $testsToRun += $arg
+        }
+    }
+}
+
+if ($testsToRun.Count -eq 0) {
+    # If no specific tests were provided, run all databases
+    $testsToRun = $Databases
+}
+
 # Run tests for each database type
-foreach ($Database in $Databases) {
+foreach ($Database in $testsToRun) {
+
+    # Check if the argument is a valid Docker database type
+    # if it is, reset the corresponding Docker volume and start the container
+    if ($DockerDatabases.ContainsKey($Database)) {
+        docker volume rm "${dirname}_queries_$$Database}_data"
+        docker-compose -f "./test-databases.docker-compose.yml" up $Database -d
+    }
+
+    $cmd = "go test -tags=`"$Database`" --timeout=30s"
+    if ($flags.verbose) {
+        $cmd += " -v"
+    }
+    if ($flags.failslow -eq $false) {
+        $cmd += " --failfast"
+    }
+    
+    $cmd += " ./..."
+
     Write-Host "Running tests for $Database"
-    go test -tags="$Database" ./... --failfast
+    Write-Host "Command: $cmd"
+    Write-Host "----------------------------------------"
+    Invoke-Expression $cmd
     if ($LASTEXITCODE -ne 0) {
+        Write-Host "----------------------------------------"
         Write-Host "Tests failed for $Database"
         exit $LASTEXITCODE
     }
